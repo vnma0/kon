8ee040c6115072fa39f34d755094fcd91c2235fd
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-12-23 19:53:58 +0700

    Add queue

---
+++ util/queue.js
@@ -0,0 +1,15 @@
+import { promisify } from "util";
+import { readdir } from "fs";
+import { submitFolder } from "../config/upload";
+
+/**
+ * Return number of files in queue
+ */
+export async function getQueue() {
+    const readdirAsync = promisify(readdir);
+    const filesInQueue = await readdirAsync(submitFolder);
+    // TODO: filter folder
+    return filesInQueue.length;
+}
+
+getQueue().then(res => console.log(res));
