f30e269a0c3f18655786782680973ac35711acf7
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-12-24 14:40:34 +0700

    Update filter

--- util/queue.js
+++ util/queue.js
@@ -1,13 +1,13 @@
 import { promisify } from "util";
 import { readdir } from "fs";
 import { submitFolder } from "../config/upload";
+import { isFile } from "./parser";
 
 /**
  * Return number of files in queue
  */
 export async function getQueue() {
     const readdirAsync = promisify(readdir);
-    const filesInQueue = await readdirAsync(submitFolder);
-    // TODO: filter folder
+    const filesInQueue = (await readdirAsync(submitFolder)).filter(isFile);
     return filesInQueue.length;
 }
