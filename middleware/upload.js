8d214f7c050d43eedc42395e6063b924c0894042
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-12-27 17:37:58 +0700

    Refactor upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,6 +3,29 @@ import { uploadFolder } from "../config/folder";
 import { taskSizeLimit, codeSizeLimit } from "../config/code";
 
 /**
+ * Multer middleware wrapper to limit upload size
+ * If payload is too large, send status 413
+ * If there's other error, send status 400
+ * Else, next()
+ * @param {Multer} multerMid multer middleware
+ * @returns {Middleware} wrapped middleware
+ */
+function limitUpload(multerMid) {
+    return (req, res, next) => {
+        multerMid(req, res, (err) => {
+            const isMulterLimit =
+                err instanceof multer.MulterError &&
+                err.code === "LIMIT_FILE_SIZE";
+            if (isMulterLimit) {
+                res.sendStatus(413);
+            } else if (err) {
+                res.sendStatus(400);
+            } else next();
+        });
+    };
+}
+
+/**
  * receive task from Wafter
  * If payload is too large, send status 413
  * If there's other error, send status 400
@@ -11,8 +34,8 @@ import { taskSizeLimit, codeSizeLimit } from "../config/code";
  * @param {Response} res Express response object
  * @param {callback} next Express next middleware function
  */
-export function taskUpload(req, res, next) {
-    const task = multer({
+export const taskUpload = limitUpload(
+    multer({
         dest: uploadFolder,
         limits: {
             fileSize: taskSizeLimit,
@@ -20,19 +43,8 @@ export function taskUpload(req, res, next) {
             parts: 1,
             preservePath: true
         }
-    }).single("task");
-
-    task(req, res, (err) => {
-        if (
-            err instanceof multer.MulterError &&
-            err.code === "LIMIT_FILE_SIZE"
-        ) {
-            res.sendStatus(413);
-        } else if (err) {
-            res.sendStatus(400);
-        } else next();
-    });
-}
+    }).single("task")
+);
 
 /**
  * receive submission from Wafter
@@ -43,8 +55,8 @@ export function taskUpload(req, res, next) {
  * @param {Response} res Express response object
  * @param {callback} next Express next middleware function
  */
-export function formUpload(req, res, next) {
-    const form = multer({
+export const formUpload = limitUpload(
+    multer({
         dest: uploadFolder,
         limits: {
             fileSize: codeSizeLimit,
@@ -52,16 +64,5 @@ export function formUpload(req, res, next) {
             parts: 2,
             preservePath: true
         }
-    }).fields([{ name: "code", maxCount: 1 }, { name: "id", maxCount: 1 }]);
-
-    form(req, res, (err) => {
-        if (
-            err instanceof multer.MulterError &&
-            err.code === "LIMIT_FILE_SIZE"
-        ) {
-            res.sendStatus(413);
-        } else if (err) {
-            res.sendStatus(400);
-        } else next();
-    });
-}
+    }).fields([{ name: "code", maxCount: 1 }, { name: "id", maxCount: 1 }])
+);
