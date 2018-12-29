b4db4384a54469ad65accef0cad8e77a38281a78
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-12-29 11:36:20 +0700

    Fix default value

--- config/server.js
+++ config/server.js
@@ -6,7 +6,7 @@ export const PORT = 30000;
 /**
  * Set true if task send is required
  */
-export const taskRequired = false;
+export const taskRequired = true;
 
 /**
  * Set true to see server log
