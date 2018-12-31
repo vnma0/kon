471dc6511d7fead7985aa380a506af528d44fe4c
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-12-31 19:59:00 +0700

    Reduce asynchronous

--- util/queue.js
+++ util/queue.js
@@ -1,13 +1,18 @@
-import { promisify } from "util";
-import { readdir } from "fs";
-import { submitFolder } from "../config/upload";
+import { readdirSync } from "fs";
+import { cwd, submitFolder } from "../config/folder";
 import { isFile } from "./parser";
+import { join } from "path";
 
 /**
- * Return number of files in queue
+ * Get queue
+ * @returns {Number} number of files in queue
  */
-export async function getQueue() {
-    const readdirAsync = promisify(readdir);
-    const filesInQueue = (await readdirAsync(submitFolder)).filter(isFile);
+export function getQueue() {
+    const workDir = join(cwd, submitFolder);
+    const filesInFolder = readdirSync(workDir);
+    // TODO: Filter queue
+    const filesInQueue = filesInFolder
+        .map((file) => join(workDir, file))
+        .filter(isFile);
     return filesInQueue.length;
 }
