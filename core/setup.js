935906f4d5f00177d30a357917c22c75e04045d3
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-01-03 07:54:09 +0700

    Better handling zip file

--- core/setup.js
+++ core/setup.js
@@ -1,12 +1,13 @@
 import extract from "extract-zip";
 import { cwd } from "../config/folder";
+import { join } from "path";
 
 /**
  * Extract Tasks file to Current Working Directory
  * @param {PathLike} filePath path to zip file
  */
 export async function extractTasks(filePath) {
-    extract(filePath, { dir: cwd }, (err) => {
+    extract(filePath, { dir: join(cwd, "Tasks") }, (err) => {
         if (err) throw err;
     });
 }
