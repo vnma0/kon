3151d7067bd05f586ce4b7d4f0e786f2e44c7631
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-01-14 08:37:02 +0700

    Fix `export`

--- src/config/server.js
+++ src/config/server.js
@@ -8,7 +8,7 @@ const PORT = isNaN(process.env.PORT) ? 30000 : process.env.PORT;
 /**
  * Set true to see server log
  */
-export const logRequest = true;
+const logRequest = true;
 
 export default {
     PORT: PORT,
