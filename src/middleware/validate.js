1436fc2f7056b5bb7e85261f539dceb334582851
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-01-15 21:20:06 +0700

    Fix comment

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -28,7 +28,7 @@ export function checkInitial(req, res, next) {
 }
 
 /**
- * Check if given file is a valid source code via MIME
+ * Check if given file is a valid source code by checking if it's binary or not
  * @param {Object} file source code blob
  */
 function checkCodeType(file) {
