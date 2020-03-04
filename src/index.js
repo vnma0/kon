e20f380b3fada465c441350884d433c69de77623
Dung Tuan Nguyen <ntddebugger@gmail.com>
2020-03-04 21:47:22 +0700

    Add log watcher

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-const { writeFileSync } = require("fs");
+const { writeFile } = require("fs");
 const { join } = require("path");
 const WebSocket = require("ws");
+const chokidar = require("chokidar");
 
 const config = require("./config/server");
 const folderCfg = require("./config/folder");
-const { sepName } = require("./util/parser");
+const { sepName, parseLog } = require("./util/parser");
 const createFolder = require("./util/createFolder");
 
 function main() {
-    const ws = new WebSocket(config.address);
+    // Generate folder
+    const submitFolder = join(folderCfg.cwd, folderCfg.submitFolder);
+    const logFolder = join(folderCfg.cwd, folderCfg.logFolder);
+    createFolder(submitFolder);
+    createFolder(logFolder);
 
-    createFolder(folderCfg.submitFolder);
-    createFolder(folderCfg.logFolder);
-    const folder = folderCfg.submitFolder;
+    const ws = new WebSocket(config.address);
 
     ws.onmessage = msg => {
         // Send file to submit folder
@@ -25,9 +28,29 @@ function main() {
         console.log(`Recieved submissions ${sub.id}`);
         const [prob, ext] = sepName(sub.name);
         const file_name = `[${sub.id}][${prob}]${ext}`;
-        const code_path = join(folder, file_name);
-        writeFileSync(code_path, sub.data, { encoding: "base64" });
+        const code_path = join(submitFolder, file_name);
+        writeFile(code_path, sub.data, { encoding: "base64" }, err => {
+            if (err) {
+                const result = {
+                    id: sub.id,
+                    err: "Server error"
+                };
+                ws.send(JSON.stringify(result));
+            }
+        });
     };
+    // TODO: Ignore unrelated files
+    const logWatch = chokidar.watch(logFolder, {
+        depth: 1,
+        persistent: true,
+        awaitWriteFinish: true
+    });
+    logWatch.on("add", async path => {
+        const msg = await parseLog(path);
+        console.log(`Sending message ${msg.id}`);
+        console.log(msg);
+        ws.send(JSON.stringify(msg));
+    });
 }
 
 main();
