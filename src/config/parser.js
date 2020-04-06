be0605ded7df28da976d701de7d4fa22e63a3ce2
Dung Tuan Nguyen <ntddebugger@gmail.com>
2020-04-06 19:03:33 +0700

    comment: Add explanation on bitmask verdict

--- src/config/parser.js
+++ src/config/parser.js
@@ -4,6 +4,10 @@
  * Verdict dictionary.
  * Parser will use this to filter verdict
  * If not matching, by default, parser will use raw verdict
+ * Below is logic bits explanation
+ * 1st      is runnable ?
+ * 2nd      is run in time ?
+ * 3rd      is output correct ?
  */
 module.exports = {
     "Kết quả đúng!": 0b111,
