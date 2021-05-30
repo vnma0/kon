3ade92fbe819713457e97ae4d1c5d0facc90774d
Dung Tuan Nguyen <ntddebugger@gmail.com>
2021-05-30 20:41:54 +0700

    fix: better bit read

--- src/config/parser.js
+++ src/config/parser.js
@@ -4,35 +4,35 @@
  * Verdict dictionary.
  * Parser will use this to filter verdict
  * Below is logic bits explanation
- * 1st      is output correct?
- * 2nd      is run in time?
- * 3rd      is runnable?
- * 4th      is compilable?
+ * 1st      have correct output?
+ * 2nd      can run in time?
+ * 3rd      can run?
+ * 4th      can compile?
  * Reverse order for future expansion
  */
 module.exports = {
     "Kết quả đúng!": {
-        bit: 0b1111,
+        bit: 0b0000,
         text: "AC",
     },
     "Kết quả khớp đáp án!": {
-        bit: 0b1111,
+        bit: 0b0000,
         text: "AC",
     },
     "Kết quả KHÁC đáp án!": {
-        bit: 0b0111,
+        bit: 0b1000,
         text: "WA",
     },
     "Chạy quá thời gian": {
-        bit: 0b0011,
+        bit: 0b1100,
         text: "TLE",
     },
     "Chạy sinh lỗi": {
-        bit: 0b0001,
+        bit: 0b1110,
         text: "RTE",
     },
     default: {
-        bit: 0b0000,
-        text: "Unknown",
+        bit: -1,
+        text: "CE",
     },
 };
