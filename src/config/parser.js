0a7240bdbfaf6657fb90c581e6e19db41e481181
Dung Tuan Nguyen <ntddebugger@gmail.com>
2021-05-30 18:48:05 +0700

    fix: future-proof bits

--- src/config/parser.js
+++ src/config/parser.js
@@ -5,15 +5,17 @@
  * Parser will use this to filter verdict
  * If not matching, by default, parser will use raw verdict
  * Below is logic bits explanation
- * 1st      is runnable ?
- * 2nd      is run in time ?
- * 3rd      is output correct ?
+ * 1st      is output correct?
+ * 2nd      is run in time?
+ * 3rd      is runnable?
+ * 4th      is compilable?
+ * Reverse order for future expansion
  */
 module.exports = {
-    "Kết quả đúng!": 0b111,
-    "Kết quả khớp đáp án!": 0b111,
-    "Kết quả KHÁC đáp án!": 0b110,
-    "Chạy quá thời gian": 0b100,
-    "Chạy sinh lỗi": 0b000,
-    default: 0b000
+    "Kết quả đúng!": 0b1111,
+    "Kết quả khớp đáp án!": 0b1111,
+    "Kết quả KHÁC đáp án!": 0b0111,
+    "Chạy quá thời gian": 0b0011,
+    "Chạy sinh lỗi": 0b0001,
+    default: 0b0000
 };
